95a5502d6620f72d045cfd32b0f34d8fa803be0c
rruviro <138757113+rruviro@users.noreply.github.com>
2024-12-17 12:25:00 +0800

    additional

---
+++ lib/resources/schema/colors.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+final primary_color = Color.fromRGBO(21, 33, 34, 1); 
+final secondary_color = Color.fromRGBO(242, 242, 242, 1);
+final tertiary_color = Color.fromRGBO(174, 174, 174, 1);
+
+final widgetPricolor = const Color.fromARGB(255, 59, 59, 59);
+final widgetSeccolor = Colors.white;
+
+final primary_textColor = Colors.black;
+final secondary_textColor = Colors.white;
+final tertiary_textColor = Colors.black54;
