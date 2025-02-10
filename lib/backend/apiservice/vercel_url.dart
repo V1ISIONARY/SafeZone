11393fa62644b7afd03f2fbd0ca3ef2e02582b43
jairasolis <jairasolis030@gmail.com>
2025-02-10 18:01:34 +0800

    update vercel url

--- lib/backend/apiservice/vercel_url.dart
+++ lib/backend/apiservice/vercel_url.dart
@@ -1,4 +1,4 @@
 class VercelUrl {
   static const String mainUrl =
-      'https://safezone-flask-a3xhq44qi-faokunns-projects.vercel.app';
+      'https://safezone-flask-63s2eoeau-faokunns-projects.vercel.app';
 }
