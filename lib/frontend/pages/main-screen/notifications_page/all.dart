45398e275854d57037bb38e133a4eeb2ff71f9d5
jairasolis <jairasolis030@gmail.com>
2025-02-11 21:23:48 +0800

    placeholder for empty notifs

--- lib/frontend/pages/main-screen/notifications_page/all.dart
+++ lib/frontend/pages/main-screen/notifications_page/all.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:safezone/resources/schema/colors.dart';
 
 class All extends StatefulWidget {
   const All({super.key});
@@ -9,10 +9,41 @@ class All extends StatefulWidget {
 }
 
 class _AllState extends State<All> {
+  bool hasContent = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: Center(
+        child: hasContent ? _buildContent() : _buildPlaceholder(),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return const Text(
+      "stuffff",
+      style: TextStyle(
+          fontSize: 15, fontWeight: FontWeight.bold, color: textColor),
+    );
+  }
+
+  Widget _buildPlaceholder() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'lib/resources/images/notif1.png',
+          width: 150,
+          height: 150,
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          "No notifications",
+          style: TextStyle(fontSize: 16, color: Colors.grey),
+        ),
+      ],
     );
   }
 }
--- lib/frontend/pages/main-screen/notifications_page/unread.dart
+++ lib/frontend/pages/main-screen/notifications_page/unread.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:safezone/resources/schema/colors.dart';
 
 class Unread extends StatefulWidget {
   const Unread({super.key});
@@ -9,10 +10,41 @@ class Unread extends StatefulWidget {
 }
 
 class _UnreadState extends State<Unread> {
+  bool hasContent = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
+      body: Center(
+        child: hasContent ? _buildContent() : _buildPlaceholder(),
+      ),
+    );
+  }
+
+  Widget _buildContent() {
+    return const Text(
+      "stuffff",
+      style: TextStyle(
+          fontSize: 15, fontWeight: FontWeight.bold, color: textColor),
+    );
+  }
+
+  Widget _buildPlaceholder() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(
+          'lib/resources/images/notif1.png',
+          width: 150,
+          height: 150,
+        ),
+        const SizedBox(height: 20),
+        const Text(
+          "No notifications",
+          style: TextStyle(fontSize: 16, color: Colors.grey),
+        ),
+      ],
     );
   }
 }
