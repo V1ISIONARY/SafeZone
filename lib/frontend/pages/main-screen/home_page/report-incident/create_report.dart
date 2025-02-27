4ba26eb3a7496f88c318ebdf2b1079e3880a7d24
GlzDrlMayln <145691779+GlzDrlMayln@users.noreply.github.com>
2025-02-27 22:52:31 +0800

    fix map

--- lib/frontend/pages/main-screen/home_page/report-incident/create_report.dart
+++ lib/frontend/pages/main-screen/home_page/report-incident/create_report.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -53,138 +52,161 @@ class _CreateReportState extends State<CreateReport> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
         centerTitle: true,
         title: const CategoryText(text: "Report an Incident"),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
+            margin: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.black),
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(Icons.arrow_back, color: Colors.black, size: 10),
+          ),
+        ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Image.asset(
-                      "lib/resources/svg/alert.png",
-                      width: 60,
-                      height: 60,
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  const Flexible(
-                    child: Text(
-                      'Help others stay safe by providing details about the incident and location.',
-                      style: TextStyle(
-                        fontSize: 15,
+      body: Column(
+        children: [
+          // Map Section
+          Container(
+            height: 250, // Fixed height for the map
+            margin: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(54, 96, 125, 139),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: GoogleMap(
+                initialCameraPosition: const CameraPosition(
+                  target: LatLng(16.043859, 120.335182),
+                  zoom: 14.0,
+                ),
+                markers: _markers,
+                onMapCreated: (GoogleMapController controller) {
+                  _mapController.complete(controller);
+                },
+                onTap: (LatLng location) {
+                  setState(() {
+                    _pinnedLocation = location;
+                    _markers.clear();
+                    _markers.add(
+                      Marker(
+                        markerId: const MarkerId("pinned_location"),
+                        position: location,
+                        infoWindow: const InfoWindow(title: "Incident Location"),
                       ),
-                    ),
+                    );
+                  });
+                },
+                zoomGesturesEnabled: true,
+                scrollGesturesEnabled: true,
+                rotateGesturesEnabled: true,
+                tiltGesturesEnabled: true,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          if (_pinnedLocation != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                "Pinned Location: ${_pinnedLocation!.latitude}, ${_pinnedLocation!.longitude}",
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ),
+          const SizedBox(height: 5),
+          // Form Section
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Image.asset(
+                          "lib/resources/svg/alert.png",
+                          width: 60,
+                          height: 60,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      const Flexible(
+                        child: Text(
+                          'Help others stay safe by providing details about the incident and location.',
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  TextFieldWidget.buildTextField(
+                    controller: _descriptionController,
+                    label: "Description",
+                    hint: "Enter description",
+                    maxLines: 5,
+                    minLines: 5,
+                  ),
+                  const SizedBox(height: 30),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                        "Upload images to provide more context about the incident (optional)"),
+                  ),
+                  const SizedBox(height: 10),
+                  MultipleImages(
+                    onImagesSelected: (images) {
+                      setState(() {
+                        selectedImages = images;
+                      });
+                    },
+                  ),
+                  CustomButton(
+                    text: "Continue",
+                    onPressed: () {
+                      if (userId == null || _pinnedLocation == null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Please select a location."),
+                            backgroundColor: Colors.red,
+                          ),
+                        );
+                        return;
+                      }
+
+                      final incidentReport = IncidentReportRequestModel(
+                        userId: userId!,
+                        description: _descriptionController.text,
+                        reportDate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
+                        reportTime: DateFormat("HH:mm:ss").format(DateTime.now()),
+                        images: selectedImages, // Pass the File objects directly
+                        reportTimestamp: reportTimestamp,
+                        latitude: _pinnedLocation!.latitude,
+                        longitude: _pinnedLocation!.longitude,
+                        radius: 50.0, // Default radius (adjust if needed)
+                        name:
+                            "Incident Report ${DateTime.now().millisecondsSinceEpoch}",
+                      );
+
+                      print("🚨 Incident Report Created: $incidentReport");
+
+                      // Navigate to the review-report screen with the model
+                      context.push('/review-report', extra: incidentReport);
+                    },
                   ),
                 ],
               ),
-              const SizedBox(height: 30),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Pin the location of the incident"),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                height: 215,
-                width: 350,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(54, 96, 125, 139),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: GoogleMap(
-                    initialCameraPosition: const CameraPosition(
-                      target: LatLng(16.043859, 120.335182),
-                      zoom: 14.0,
-                    ),
-                    markers: _markers,
-                    onMapCreated: (GoogleMapController controller) {
-                      _mapController.complete(controller);
-                    },
-                    onTap: (LatLng location) {
-                      setState(() {
-                        _pinnedLocation = location;
-                        _markers.clear();
-                        _markers.add(
-                          Marker(
-                            markerId: const MarkerId("pinned_location"),
-                            position: location,
-                            infoWindow:
-                                const InfoWindow(title: "Incident Location"),
-                          ),
-                        );
-                      });
-                    },
-                    zoomGesturesEnabled: true,
-                    scrollGesturesEnabled: true,
-                    rotateGesturesEnabled: true,
-                    tiltGesturesEnabled: true,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              TextFieldWidget.buildTextField(
-                controller: _descriptionController,
-                label: "Description",
-                hint: "Enter description",
-                maxLines: 5,
-                minLines: 5,
-              ),
-              const SizedBox(height: 30),
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                    "Upload images to provide more context about the incident (optional)"),
-              ),
-              const SizedBox(height: 10),
-              MultipleImages(
-                onImagesSelected: (images) {
-                  setState(() {
-                    selectedImages = images;
-                  });
-                },
-              ),
-              CustomButton(
-                text: "Continue",
-                onPressed: () {
-                  if (userId == null || _pinnedLocation == null) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text("Please select a location."),
-                        backgroundColor: Colors.red,
-                      ),
-                    );
-                    return;
-                  }
-
-                  final incidentReport = IncidentReportRequestModel(
-                    userId: userId!,
-                    description: _descriptionController.text,
-                    reportDate: DateFormat("yyyy-MM-dd").format(DateTime.now()),
-                    reportTime: DateFormat("HH:mm:ss").format(DateTime.now()),
-                    images: selectedImages, // Pass the File objects directly
-                    reportTimestamp: reportTimestamp,
-                    latitude: _pinnedLocation!.latitude,
-                    longitude: _pinnedLocation!.longitude,
-                    radius: 50.0, // Default radius (adjust if needed)
-                    name:
-                        "Incident Report ${DateTime.now().millisecondsSinceEpoch}",
-                  );
-
-                  print("🚨 Incident Report Created: $incidentReport");
-
-                  // Navigate to the review-report screen with the model
-                  context.push('/review-report', extra: incidentReport);
-                },
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
