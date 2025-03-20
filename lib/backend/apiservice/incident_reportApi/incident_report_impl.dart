3086924f0c5501fb0dd948d198efa89625aa314f
jairasolis <jairasolis030@gmail.com>
2025-03-20 23:01:07 +0800

    fixed incident report creation functionality

--- lib/backend/apiservice/incident_reportApi/incident_report_impl.dart
+++ lib/backend/apiservice/incident_reportApi/incident_report_impl.dart
@@ -96,15 +96,45 @@ class IncidentRepositoryImpl implements IncidentReportRepository {
   @override
   Future<IncidentReportRequestModel> createIncidentReport(
       IncidentReportRequestModel incidentReport) async {
-    print("Request Body: ${jsonEncode(incidentReport.toJson())}");
+    var request = http.MultipartRequest('POST', Uri.parse('$_apiUrl/incident'));
 
-    final response = await http.post(
-      Uri.parse('$_apiUrl/incident'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(incidentReport.toJson()),
-    );
+    request.fields['user_id'] = incidentReport.userId.toString();
+    if (incidentReport.dangerZoneId != null) {
+      request.fields['danger_zone_id'] = incidentReport.dangerZoneId.toString();
+    }
+    request.fields['description'] = incidentReport.description ?? '';
+    request.fields['report_date'] = incidentReport.reportDate ?? '';
+    request.fields['report_time'] = incidentReport.reportTime ?? '';
+    request.fields['report_timestamp'] = incidentReport.reportTimestamp ?? '';
+    request.fields['latitude'] = incidentReport.latitude.toString();
+    request.fields['longitude'] = incidentReport.longitude.toString();
+    request.fields['radius'] = incidentReport.radius.toString();
+    request.fields['name'] = incidentReport.name ?? '';
+
+    if (incidentReport.images != null && incidentReport.images!.isNotEmpty) {
+      for (int i = 0; i < incidentReport.images!.length; i++) {
+        var file = incidentReport.images![i];
+        var stream = http.ByteStream(file.openRead());
+        var length = await file.length();
+
+        String filename =
+            'image_${i}_${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+        var multipartFile = http.MultipartFile(
+          'images', 
+          stream,
+          length,
+          filename: filename,
+        );
+
+        request.files.add(multipartFile);
+      }
+    }
+
+    print("Sending request with ${incidentReport.images?.length ?? 0} images");
+
+    var streamedResponse = await request.send();
+    var response = await http.Response.fromStream(streamedResponse);
 
     print("Response Status Code: ${response.statusCode}");
     print("Response Body: ${response.body}");
@@ -114,19 +144,14 @@ class IncidentRepositoryImpl implements IncidentReportRepository {
 
       final Map<String, dynamic> responseData = {
         'id': data['incident_report_id'],
-        'user_id': incidentReport.userId, 
-        'danger_zone_id':
-            incidentReport.dangerZoneId, 
-        'description':
-            incidentReport.description,
-        'report_date':
-            incidentReport.reportDate, 
-        'report_time':
-            incidentReport.reportTime, 
-        'report_timestamp':
-            incidentReport.reportTimestamp,
-        'latitude': incidentReport.latitude, 
-        'longitude': incidentReport.longitude, 
+        'user_id': incidentReport.userId,
+        'danger_zone_id': incidentReport.dangerZoneId,
+        'description': incidentReport.description,
+        'report_date': incidentReport.reportDate,
+        'report_time': incidentReport.reportTime,
+        'report_timestamp': incidentReport.reportTimestamp,
+        'latitude': incidentReport.latitude,
+        'longitude': incidentReport.longitude,
         'radius': incidentReport.radius,
         'name': incidentReport.name,
       };
